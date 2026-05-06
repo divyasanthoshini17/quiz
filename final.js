e219f31d98e0120c96123db5e5cb302aabdedee1
divyasanthoshini4 <divyasanthoshini4@gmail.com>
2026-05-06 21:08:57 +0530

    attemptschema and save-result

--- final.js
+++ final.js
@@ -1,10 +1,14 @@
 var express=require("express");
 var app=express();
+var bodyParser=require("body-parser");
+app.use(bodyParser.json());
 require("dotenv").config();
 var cors=require("cors");
 app.use(cors());
 var dbConnect = require("./db");
 dbConnect();
+const Attempt = require("./models/Attempt");
+
 app.get("/api/quiz",(req,res)=>{
     // res.send("hello world")
     fetch("https://the-trivia-api.com/api/questions?limit=5")
@@ -13,6 +17,17 @@ app.get("/api/quiz",(req,res)=>{
     .catch(err => res.status(500).json({ error: "Failed to fetch quiz" }));
 })
 
+
+app.post("/api/save-result", async (req, res) => {
+    const { score, total } = req.body;
+    
+    const newAttempt = new Attempt({
+        score,
+        total,
+    });
+
+    await newAttempt.save();
+});
 app.listen(process.env.PORT||3000,()=>{
     console.log("server uriking on " + process.env.PORT)
 })
